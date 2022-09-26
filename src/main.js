77491be5886ec54b4f7ff413884c3e7ac0db8570
Romain <r.martineau@manitou-group.com>
2022-09-26 09:52:16 +0200

    init project

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import { createAuth0 } from '@auth0/auth0-vue';
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+
+import Button from 'primevue/Button';
+
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'                 
+import 'primeicons/primeicons.css'  
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.component('Button', Button);
+
+app.use(
+    createAuth0({
+      domain: "dev-v1aswrmd.eu.auth0.com",
+      client_id: "bow8Kx0Rg5nNBwBZoC5mJYDNvHiwNUcB",
+      redirect_uri: window.location.origin
+    })
+  );
+
+app.use(router)
+
+app.use(PrimeVue);
+
+app.mount('#app')
---
+++ src/views/UserView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <h2>User Profile</h2>
+      <pre v-if="isAuthenticated">
+        <code>{{ user }}</code>
+      </pre>
+      <Button v-else class="p-button-primary" @click="login">Log in</Button>
+    </div>
+    <Button class="p-button-primary" @click="logout">Log out</Button>
+  </div>
+</template>
+<script>
+import { useAuth0 } from "@auth0/auth0-vue";
+
+export default {
+  setup() {
+    const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
+    return {
+      login: () => {
+        loginWithRedirect();
+      },
+      logout: () => {
+        logout({ returnTo: window.location.origin });
+      },
+      login: () => {
+          loginWithRedirect();
+        },
+        user,
+        isAuthenticated
+    };
+  },
+};
+</script>
