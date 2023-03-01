5e8ba917ec6fa566a0b34fd9ad91e24839f5ca5d
Romain <r.martineau@manitou-group.com>
2023-03-01 14:21:36 +0100

    autho

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Button icon="pi pi-arrow-right" @click="visibleLeft = true" class="mr-2" />
+
+        <Sidebar v-model:visible="visibleLeft" :baseZIndex="10000">
+            <h3>Left Sidebar</h3>
+        </Sidebar>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            visibleLeft: false,
+        }
+    }
+}
+</script>
